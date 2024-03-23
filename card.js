44fa4027a97dc527941e54fd78dc4bf276a7d431
Breno Yohan <breno@zettaware.com.br>
2024-03-22 21:03:15 -0300

    first commit

---
+++ card.js
@@ -0,0 +1,13 @@
+const id = document.getElementById('guia')
+
+id.addEventListener('click', go)
+
+function go(){
+    
+    window.location.href = "https://www.youtube.com/watch?v=Ejkb_YpuHWs&list=PLHz_AreHm4dkZ9-atkcmcBaMZdmLHft8n"
+
+}
+
+
+
+
